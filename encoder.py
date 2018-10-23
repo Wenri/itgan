27a086f0a361b6d9e75469c37e5b5cbab7198e31
Bingchen Gong <Wenri@users.noreply.github.com>
2018-10-23 17:09:13 +0800

    Add ResNet&WRN, WeightNorm in WRN untested.

--- encoder.py
+++ encoder.py
@@ -4,60 +4,20 @@ from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, GlobalM
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, BatchNormalization
 from tensorflow.keras import regularizers
 from wn import WeightNorm
-
+from resnet import resnet_v2
+from wrn import build_model as wrn
 
 class Encoder(Model):
     def __init__(self, weight_decay, num_classes):
         super(Encoder, self).__init__(name='Encoder')
-        self.model = Sequential([
-            WeightNorm(Conv2D(128, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))),
-            Activation('relu'),
-            BatchNormalization(),
-            WeightNorm(Conv2D(128, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))),
-            Activation('relu'),
-            BatchNormalization(),
-            WeightNorm(Conv2D(128, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))),
-            Activation('relu'),
-            BatchNormalization(),
-            Dropout(0.5),
-
-            MaxPooling2D(pool_size=(2, 2)),
-
-            WeightNorm(Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))),
-            Activation('relu'),
-            BatchNormalization(),
-            WeightNorm(Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))),
-            Activation('relu'),
-            BatchNormalization(),
-            WeightNorm(Conv2D(256, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))),
-            Activation('relu'),
-            BatchNormalization(),
-            Dropout(0.5),
-
-            MaxPooling2D(pool_size=(2, 2)),
-
-            WeightNorm(Conv2D(512, (3, 3), padding='same',kernel_regularizer=regularizers.l2(weight_decay))),
-            Activation('relu'),
-            BatchNormalization(),
-            WeightNorm(Conv2D(256, (1, 1), padding='same',kernel_regularizer=regularizers.l2(weight_decay))),
-            Activation('relu'),
-            BatchNormalization(),
-            WeightNorm(Conv2D(128, (1, 1), padding='same',kernel_regularizer=regularizers.l2(weight_decay))),
-            Activation('relu'),
-            BatchNormalization(),
-            Dropout(0.5),
-
-            Flatten(),
-            WeightNorm(Dense(1024)),
-            Activation('relu'),
-            BatchNormalization(),
-            Dropout(0.5),
-            WeightNorm(Dense(128)),
-            Activation('relu'),
-            BatchNormalization(),
-        ])
-        self.cls_out = WeightNorm(Dense(num_classes))
+        # n = 12
+        # depth = n * 9 + 2
+        # self.model = resnet_v2((32, 32, 3), depth)
+        self.model = wrn((32, 32, 3), dropout=0.2)
+        self.metrics_out = GlobalMaxPooling2D()
+        self.cls_out = Dense(num_classes)
 
     def call(self, inputs, training=None, mask=None):
-        metric_out = self.model(inputs, training=training)
+        feature_map = self.model(inputs, training=training)
+        metric_out = self.metrics_out(feature_map)
         return metric_out, self.cls_out(metric_out)
