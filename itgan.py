c503f5be354a29c0b54ff7107333762e1236cdde
Bingchen Gong <wenri@Bingchens-MacBook-Air.local>
2018-11-09 16:41:16 +0800

    idea change

--- itgan.py
+++ itgan.py
@@ -48,6 +48,10 @@ class cifar10vgg:
         self.weight_decay = 0.0005
         self.x_shape = (32, 32, 3)
 
+        # these values produced during first training and are general for the standard cifar10 training set normalization
+        self.mean = 120.707
+        self.std = 64.15
+
         self.model, self.decoder = self.build_model()
 
         if train:
@@ -60,10 +64,10 @@ class cifar10vgg:
         # it is used when training a model.
         # Input: training set and test set
         # Output: normalized training set and test set according to the trianing set statistics.
-        mean = np.mean(X_train,axis=(0,1,2,3))
-        std = np.std(X_train, axis=(0, 1, 2, 3))
-        X_train = (X_train-mean)/(std+1e-7)
-        X_test = (X_test-mean)/(std+1e-7)
+        self.mean = np.mean(X_train,axis=(0,1,2,3))
+        self.std = np.std(X_train, axis=(0, 1, 2, 3))
+        X_train = (X_train-self.mean)/(self.std+1e-7)
+        X_test = (X_test-self.mean)/(self.std+1e-7)
         return X_train, X_test
 
     def normalize_production(self, x):
@@ -71,10 +75,10 @@ class cifar10vgg:
         # Input: X - a training set
         # Output X - a normalized training set according to normalization constants.
 
-        # these values produced during first training and are general for the standard cifar10 training set normalization
-        mean = 120.707
-        std = 64.15
-        return (x-mean)/(std+1e-7)
+        return (x-self.mean)/(self.std+1e-7)
+
+    def normalize_reverse(self, x):
+        return x * (self.std+1e-7) + self.mean
 
     def build_model(self):
         encoder = Encoder(self.x_shape, self.num_classes, self.weight_decay)
@@ -94,7 +98,7 @@ class cifar10vgg:
             _, test_pred[b:b+bsize], z_avg, z_log_var = self.model(tf.convert_to_tensor(x[b:b+bsize]), training=False)
             recon[b:b+bsize] = self.decoder((z_avg, z_log_var), training=False)
 
-        return test_pred, recon
+        return test_pred, self.normalize_reverse(recon)
 
     def train(self, model, decoder):
 
@@ -203,7 +207,7 @@ class cifar10vgg:
                 accuracy = np.mean(test_pred == test_gt)
                 print("epoch = %.2f, loss = %f (recon = %f), train_accuracy = %.4f, test_accuracy = %.4f" % 
                     (total_samples / num_data, total_losses * batch_size / batch_samples, total_recon * batch_size / batch_samples, correct / batch_samples, accuracy))
-                save_images(test_recon, "test_images_step_%d.png" % global_step)
+                save_images(test_recon / 255.0, "results/test_images_step_%d.png" % global_step)
                 total_losses = 0.0
                 total_recon = 0.0
                 correct = 0
